3d163c615b24dd43e2412f8ea60abd895df99f05
papple23g <papple23g@gmail.com>
2023-04-17 17:59:43 +0800

    refactor: ♻get_prmote_page_url_list

--- libs/uber_eats.py
+++ libs/uber_eats.py
@@ -13,6 +13,7 @@ from .chrome import Chrome
 
 
 class UberEats:
+    MAIN_PAGE_URL = "https://www.ubereats.com"
     browser = None
     page = None
 
@@ -29,19 +30,19 @@ class UberEats:
             Set[str]: 優惠碼集合
         """
         logger.info('進入 UE 首頁並等待頁面加載完成 ...')
-        ue_main_page_url = "https://www.ubereats.com"
-        await cls.page.goto(ue_main_page_url, timeout=0)
-        a_list_selector_str = "#main-content > div > div > div:nth-child(2) > div > div > div > div > div > div > div:nth-child(3) a"
-        await cls.page.wait_for_selector(a_list_selector_str, timeout=0)
+        await cls.page.goto(cls.MAIN_PAGE_URL, timeout=0)
+        a_list_locator = cls.page.locator(
+            "#main-content > div > div > div:nth-child(2) > div > div > div > div > div > div > div:nth-child(3) a"
+        )
+        await a_list_locator.first.wait_for(timeout=0)
 
         logger.info('獲取優惠碼頁面連結串列 ...')
-        a_list_locator = cls.page.locator(a_list_selector_str)
         a_list_len = await a_list_locator.count()
         url_list = []
         for a_i in range(a_list_len):
-            url = await a_list_locator.nth(a_i).get_attribute('href')
-            if ('marketing?bbid' in url):
-                url_list.append(f"{ue_main_page_url}{url}")
+            href_str = await a_list_locator.nth(a_i).get_attribute('href')
+            if ('marketing?bbid' in href_str):
+                url_list.append(f"{cls.MAIN_PAGE_URL}{href_str}")
         return url_list
 
     @classmethod
@@ -55,8 +56,9 @@ class UberEats:
 
             logger.info('獲取優惠碼頁面連結串列 ...')
             prmote_page_url_list = await cls.get_prmote_page_url_list()
-            for prmote_page_url in prmote_page_url_list:
-                print(prmote_page_url_list)
+            print((prmote_page_url_list))
+            # for prmote_page_url in prmote_page_url_list:
+            # print(prmote_page_url_list)
 
         # await save_cookies_json()
         # await test_google_without_login()
